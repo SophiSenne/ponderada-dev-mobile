5ddfd63d873fb9e69227d8ccae301a192a8da8d2
SophiSenne <Sophia.Silva@sou.inteli.edu.br>
2026-05-08 11:37:55 -0300

    feat: adiciona outros dados e imagem pro d06

--- app/src/main/java/carvalho/zanini/ponderada1/MainActivity.kt
+++ app/src/main/java/carvalho/zanini/ponderada1/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlin.random.Random
+import androidx.compose.foundation.Image
 
 import androidx.compose.ui.tooling.preview.Preview
 import carvalho.zanini.ponderada1.ui.theme.Ponderada1Theme
@@ -28,8 +30,9 @@ class MainActivity : ComponentActivity() {
 fun LancadorDeDadosApp() {
     var dadoSelecionado by remember { mutableStateOf("D6") }
     var resultado by remember { mutableStateOf("Clique no botão para lançar o dado") }
+    var valorSorteado by remember {mutableStateOf(0)}
 
-    val dados = listOf("D6",)
+    val dados = listOf("D6", "D10", "D20", "D100",)
 
     Column(
         modifier = Modifier
@@ -60,15 +63,18 @@ fun LancadorDeDadosApp() {
         }
 
         Spacer(modifier = Modifier.height(16.dp))
-
         Button(
             onClick = {
-                val valorSorteado = when (dadoSelecionado) {
-                    "D6" -> Random.nextInt(6)
+                valorSorteado = when (dadoSelecionado) {
+                    "D6" -> Random.nextInt(1, 7)
+                    "D10" -> Random.nextInt(1, 11)
+                    "D20" -> Random.nextInt(1, 21)
+                    "D100" -> Random.nextInt(1, 101)
                     else -> 0
                 }
 
                 resultado = "Resultado do $dadoSelecionado: $valorSorteado"
+
             },
             modifier = Modifier.fillMaxWidth()
         ) {
@@ -81,5 +87,29 @@ fun LancadorDeDadosApp() {
             text = resultado,
             fontSize = 20.sp
         )
+
+        Image(
+            painter = painterResource(
+                id = obterImagemDado(dadoSelecionado, valorSorteado)
+            ),
+            contentDescription = "Face do dado",
+            modifier = Modifier.size(200.dp)
+        )
+    }
+}
+
+fun obterImagemDado(tipo: String, valor: Int): Int {
+    return when (tipo) {
+
+        "D6" -> when (valor) {
+            1 -> R.drawable.inverted_dice_1
+            2 -> R.drawable.inverted_dice_2
+            3 -> R.drawable.inverted_dice_3
+            4 -> R.drawable.inverted_dice_4
+            5 -> R.drawable.inverted_dice_5
+            else -> R.drawable.inverted_dice_6
+        }
+
+        else -> R.drawable.ic_launcher_foreground
     }
 }
